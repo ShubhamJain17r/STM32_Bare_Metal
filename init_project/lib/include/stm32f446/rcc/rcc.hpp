dd0eecc96bae3b864807cf891ee55b62e8b3a7e2
Shubham Jain <shubhamjain00050@gmail.com>
2026-07-02 21:17:39 +0530

    Update rcc.hpp

--- init_project/lib/include/stm32f446/rcc/rcc.hpp
+++ init_project/lib/include/stm32f446/rcc/rcc.hpp
@@ -1,25 +1,24 @@
+#pragma once
+
 #include "stm32f446xx.h"
 #include <cstdint>
 
-#include "stm32f446/common/Bit.hpp"
-#include "stm32f446/common/RegisterUtils.hpp"
-
 namespace stm32f446
 {
-
 namespace rcc
 {
 
 namespace gpio
 {
 	void enableClock(GPIO_TypeDef *port);
-};	// namespace gpio
+	bool isClockEnabled(GPIO_TypeDef *port);
+}	// namespace gpio
 
 namespace syscfg
 {
-	void enableClock(void);
-};	// namespace syscfg
+	void enableClock();
+	bool isClockEnabled();
+}	// namespace syscfg
 
-};	// namespace rcc
-
-};	// namespace stm32f446
+}	// namespace rcc
+}	// namespace stm32f446
